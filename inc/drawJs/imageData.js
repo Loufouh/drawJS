e2499b34ef10d28f403912c5683bef0f29f13ca4
Louis <louis.masson@palissy.fr>
2018-12-09 17:29:23 +0100

    Add getImageData() :sparkles:

--- inc/drawJs/imageData.js
+++ inc/drawJs/imageData.js
@@ -4,6 +4,10 @@ function createImageData(width, height, ctx=targetContext) {
 	return ctx.createImageData(width, height, ctx);
 }
 
+function getImageData(x, y, width, height, ctx=targetContext) {
+	return ctx.getImageData(x, y, width, height);
+}
+
 function putImageData(imgData, x, y, ctx=targetContext) {
 	ctx.putImageData(imgData, x, y);
 }
