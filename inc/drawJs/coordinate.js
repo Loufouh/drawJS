3ad6bb17b8e4b30a6dfe70dc9d6501e885dd1ae5
Louis Masson <louis.masson@etudiant.univ-lr.fr>
2021-08-08 00:44:38 +0200

    :recycle: Refactor coordinate.js

--- inc/drawJs/coordinate.js
+++ inc/drawJs/coordinate.js
@@ -1,29 +1,83 @@
 "use strict";
 
-function resetTransform(ctx=targetContext) {
-	setTransform(1, 0, 0, 1, 0, 0, ctx);
+/**
+ * Reset all transformations
+ * @param ctx The canvas context.
+ */
+function resetTransform(ctx = targetContext) {
+    setTransform(
+        1,
+        0,
+        0,
+        1,
+        0,
+        0,
+        ctx
+    );
 }
 
-function setTransform(hScal, hSkew, vSkew, vScal, hMov, vMov, ctx=targetContext) {
-	ctx.setTransform(hScal, hSkew, vSkew, vScal, hMov, vMov);
+/**
+ * Apply a transformation.
+ * @param horizontalScaling The horizontal scaling.
+ * @param horizontalSkewing The horizontal skewing.
+ * @param verticalSkewing The vertical skewing.
+ * @param verticalScaling The vertical scaling.
+ * @param horizontalTranslation The vertical translation.
+ * @param verticalTranslation The vertical translation.
+ * @param ctx The context.
+ */
+function setTransform(horizontalScaling, horizontalSkewing, verticalSkewing, verticalScaling, horizontalTranslation, verticalTranslation, ctx = targetContext) {
+    ctx.setTransform(
+        horizontalScaling,
+        horizontalSkewing,
+        verticalSkewing,
+        verticalScaling,
+        horizontalTranslation,
+        verticalTranslation
+    );
 }
 
-function translate(x, y, ctx=targetContext) {
-	ctx.translate(x, y);
+/**
+ * Apply a translation.
+ * @param x The x translation.
+ * @param y The y translation.
+ * @param ctx The context.
+ */
+function translate(x, y, ctx = targetContext) {
+    ctx.translate(x, y);
 }
 
-function rotate(angle, ctx=targetContext) {
-	ctx.rotate(angle);
+/**
+ * Apply a clockwise rotation (the rotation point is the origin of the canvas, we can use a translation to change it).
+ * @param angle The angle of the rotation (in radians).
+ * @param ctx The context.
+ */
+function rotate(angle, ctx = targetContext) {
+    ctx.rotate(angle);
 }
 
-function scale(x, y, ctx=targetContext) {
-	ctx.scale(x, y);	
+/**
+ * Apply a scaling transformation.
+ * @param x The x scaling.
+ * @param y The y scaling.
+ * @param ctx The context.
+ */
+function scale(x, y, ctx = targetContext) {
+    ctx.scale(x, y);
 }
 
-function save(ctx=targetContext) {
-	ctx.save();
+/**
+ * Save the current transformations (use restore() to get it back).
+ * @param ctx The context.
+ */
+function save(ctx = targetContext) {
+    ctx.save();
 }
 
-function restore(ctx=targetContext) {
-	ctx.restore();	
+/**
+ * Restore transformations (previously registered with save()).
+ * @param ctx The context.
+ */
+function restore(ctx = targetContext) {
+    ctx.restore();
 }
