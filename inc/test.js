20c5d87780187c47c9ee745599e3cf6919245d5d
Louis Masson <louis.masson@etudiant.univ-lr.fr>
2021-08-07 23:50:48 +0200

    :recycle: Refactor inc/test.js

--- inc/test.js
+++ inc/test.js
@@ -2,43 +2,81 @@
 
 let canvas;
 
-window.onload = init;
+window.onload = function () {
+    canvas = document.querySelector("canvas");
+    setTargetContext(canvas.getContext("2d"));
 
-function init() {
-	canvas = document.querySelector("canvas");
-	setTargetContext(canvas.getContext("2d"));
+    background(Color.hsl(200, 100, 10));
 
-	background(Color.hsl(200, 100, 10));
-	stroke(Color.rgb(255));
-	strokeWeight(3);
-	lineJoin('round');
-
-	grid(new Vector(100, 100), new Vector(51, 51), new Vector(0, 0), new Vector(400, 400))
-
-	lineDashOffset(2);
-	setLineDash([5, 10]);
-
-	line(10, 10, 600, 100);
-
-	let imgData = createImageData(100, 500);
-
-	mapImageData(imgData, pickColor);
-	putImageData(imgData, 600, 100);
-
-	let linearGradient = createRadialGradient(300, 600, 20, 300, 600, 90);
-	
-	linearGradient.addColorStop(0, 'rgb(255, 10, 10)');
-	linearGradient.addColorStop(0.1, 'rgb(100, 255, 10)');
-	linearGradient.addColorStop(1, 'hsl(200, 100%, 10%)');
-	
-	noStroke();
-	fillGradient(linearGradient);
-
-	circle(300, 600, 90);
+    drawGridTest();
+    drawLineTest();
+    drawImageDataTest();
+    drawGradientTest();
 }
 
+function drawGridTest() {
+    stroke(Color.rgb(255));
+    strokeWeight(3);
+    lineJoin('round');
+
+    grid(
+        new Vector(100, 100),
+        new Vector(51, 51),
+        new Vector(0, 0),
+        new Vector(400, 400)
+    )
+}
+
+function drawLineTest() {
+    stroke(Color.rgb(255));
+    strokeWeight(3);
+
+    lineDashOffset(2);
+    setLineDash([5, 10]);
+
+    line(
+        10,
+        10,
+        600,
+        100
+    );
+}
+
+function drawImageDataTest() {
+    let imgData = createImageData(100, 500);
+
+    mapImageData(imgData, pickColor);
+    putImageData(imgData, 600, 100);
+}
+
+function drawGradientTest() {
+    let linearGradient = createRadialGradient(
+        300,
+        600,
+        20,
+        300,
+        600,
+        90
+    );
+
+    linearGradient.addColorStop(0, 'rgb(255, 10, 10)');
+    linearGradient.addColorStop(0.1, 'rgb(100, 255, 10)');
+    linearGradient.addColorStop(1, 'hsl(200, 100%, 10%)');
+
+    fillGradient(linearGradient);
+    noStroke();
+
+    circle(300, 600, 90);
+}
+
+/**
+ * Pick a random color.
+ * @returns {Color} The picked color.
+ */
 function pickColor() {
-	return new Color( 51*Math.floor(Math.random()*6),
-			  51*Math.floor(Math.random()*6),
-			  51*Math.floor(Math.random()*6) );
+    return new Color(
+        51 * Math.floor(Math.random() * 6),
+        51 * Math.floor(Math.random() * 6),
+        51 * Math.floor(Math.random() * 6)
+	);
 }
