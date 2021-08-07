308a5f67717209f211368775da5f4d089c40a61d
Louis Masson <louis.masson@etudiant.univ-lr.fr>
2021-08-07 23:54:44 +0200

    :arrow_up: Update MathJS

--- inc/math/Vector.js
+++ inc/math/Vector.js
@@ -1,24 +1,117 @@
 "use strict";
 
+/**
+ * Represents a couple a values.
+ */
 class Vector {
+	/**
+	 * The constructor.
+	 * @param x The x component.
+	 * @param y The y component.
+	 */
 	constructor(x, y) {
 		this.x = x;
 		this.y = y;
 	}
 
-	static add(v, w) {
-		return new Vector(v.x + w.x, v.y + v.y);
+	/**
+	 * Add a vector.
+	 * @param v The vector to add.
+	 */
+	add(v) {
+		let w = Vector.add(this, v);
+
+		this.x = w.x;
+		this.y = w.y;
 	}
 
-	static substract(v, w) {
+	/**
+	 * Subtract a vector.
+	 * @param v The vector to subtract.
+	 */
+	subtract(v) {
+		let w = Vector.subtract(this, v);
+
+		this.x = w.x;
+		this.y = w.y;
+	}
+
+	/**
+	 * Multiply by a value.
+	 * @param value The value to multiply.
+	 */
+	multiply(value) {
+		let v = Vector.multiply(this, value);
+
+		this.x = v.x;
+		this.y = v.y;
+	}
+
+	/**
+	 * Divide by a value.
+	 * @param value The value to divide.
+	 */
+	divide(value) {
+		let v = Vector.divide(this, value);
+
+		this.x = v.x;
+		this.y = v.y;
+	}
+
+	/**
+	 * Copy to a new Vector.
+	 * @returns {Vector} The copy.
+	 */
+	copy() {
+		return new Vector(this.x, this.y);
+	}
+
+	/**
+	 * Verify if two vectors are equals (by '==' operator)
+	 * @param v The vector to compare
+	 * @returns {boolean} True if vectors are equals, false otherwise.
+	 */
+	equals(v) {
+		return this.x == v.x && this.y == v.y;
+	}
+
+	/**
+	 * Add two vectors
+	 * @param v The first vector.
+	 * @param w The second vector.
+	 * @returns {Vector} The resulting vector.
+	 */
+	static add(v, w) {
+		return new Vector(v.x + w.x, v.y + w.y);
+	}
+
+	/**
+	 * Subtract two vectors.
+	 * @param v The first vector.
+	 * @param w The second vector.
+	 * @returns {Vector} The resulting vector.
+	 */
+	static subtract(v, w) {
 		return new Vector(v.x - w.x, v.y - w.y);
 	}
 
+	/**
+	 * Multiply a vector by a value.
+	 * @param v The vector.
+	 * @param value The value.
+	 * @returns {Vector} The resulting vector.
+	 */
 	static multiply(v, value) {
 		return new Vector(v.x*value, v.y*value);
 	}
 
-	static multiply(v, value) {
+	/**
+	 * Divide a vector by a value.
+	 * @param v The vector.
+	 * @param value The value.
+	 * @returns {Vector} The resulting vector.
+	 */
+	static divide(v, value) {
 		return new Vector(v.x/value, v.y/value);
 	}
 }
--- inc/math/random.js
+++ inc/math/random.js
@@ -1,13 +1,21 @@
 "use strict";
 
+/**
+ * Pick a random integer between two values.
+ * @param min The lower boundary.
+ * @param max The higher boundary.
+ * @returns {number} The random integer.
+ */
 function randomInt(min, max) {
 	return Math.floor(random(min, max));
 }
 
+/**
+ * Pick a random value between two values.
+ * @param min The lower boundary.
+ * @param max The hihger boundary.
+ * @returns {any} The  random value.
+ */
 function random(min, max) {
 	return Math.random() * (max - min) + min; 
-}
-
-function randomChar() {
-	return String.fromCharCode(randomInt(100, 500));
 }
