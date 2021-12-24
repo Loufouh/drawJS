a86735d6ffe36dd3718dc3fcda2e36d3649d762e
louis700 <louis.masson@etudiant.univ-lr.fr>
2021-12-24 16:53:46 +0100

    :package: Add packages in order to the test page to work.

---
+++ node_modules/@louis700/mathjs/index.js
@@ -0,0 +1,11 @@
+import * as math from "./Math.js";
+import * as vector from "./Vector.js";
+import * as random from "./random.js";
+import * as graphDrawer from "./GraphDrawer.js";
+
+export {
+    math,
+    vector,
+    random,
+    graphDrawer
+};
