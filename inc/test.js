e71edda4308503839a33f1e739b40288ca4094e1
louis700 <louis.masson.contact@gmail.com>
2022-01-20 17:10:28 +0100

    :sparkles: Add shapes.shape() function

--- inc/test.js
+++ inc/test.js
@@ -3,15 +3,20 @@ import {vector} from "../node_modules/@louis700/mathjs/index.js";
 
 "use strict";
 
+const PURPLE = new color.Color(200, 16, 255);
+const WHITE = new color.Color(255);
+const BACKGROUND_COLOR = new color.Color(0, 34, 51);
+
 let canvas;
 
 window.onload = function () {
     canvas = document.querySelector("canvas");
     drawJS.setTargetContext(canvas.getContext("2d"));
 
-    style.background(color.Color.hsl(200, 100, 10));
+    style.background(BACKGROUND_COLOR);
 
     drawGridTest();
+    drawShapeTest();
     drawLineTest();
     drawImageDataTest();
     drawGradientTest();
@@ -20,7 +25,7 @@ window.onload = function () {
 }
 
 function drawGridTest() {
-    style.stroke(color.Color.rgb(255));
+    style.stroke(WHITE);
     shapes.strokeWeight(3);
     style.lineJoin('round');
 
@@ -32,7 +37,7 @@ function drawGridTest() {
 }
 
 function drawLineTest() {
-    style.stroke(color.Color.rgb(255));
+    style.stroke(WHITE);
     shapes.strokeWeight(3);
 
     style.lineDashOffset(2);
@@ -51,6 +56,32 @@ function drawLineTest() {
         600,
         600
     );
+}
+
+function drawShapeTest() {
+    style.stroke(PURPLE);
+    style.noFill();
+
+    shapes.strokeWeight(2);
+
+    coordinate.translate(350, 600);
+
+    let points = new Array(1000)
+                        .fill(1)
+                        .map((p, i) => {
+                            let angle = i*2*Math.PI/1000;
+                            let radius = 70 + 5*Math.cos(i/5)*3*Math.sin(angle) + 3*Math.cos(i/10)*Math.sin(angle);
+
+                            return new vector.Vector(
+                                radius*Math.cos(angle),
+                                radius*Math.sin(angle)
+                            );
+                        });
+    console.log("points:", points);
+
+    shapes.shape(points);
+
+    coordinate.resetTransform();
 }
 
 function drawImageDataTest() {
@@ -87,13 +118,13 @@ function drawImageTest() {
     img.addEventListener("load", () => {
         shapes.drawImage(
             img,
-            canvas.width - 1.8*80,
+            canvas.width - 1.8 * 80,
             10,
             144,
             80
         );
 
-        coordinate.translate(1.8*80, canvas.height);
+        coordinate.translate(1.8 * 80, canvas.height);
         coordinate.rotate(Math.PI);
 
         shapes.drawImage(
@@ -110,7 +141,7 @@ function drawImageTest() {
 
 function drawTextTest() {
     style.noFill();
-    style.stroke(new color.Color(200, 16, 255));
+    style.stroke(PURPLE);
     style.lineJoin('round')
     style.setLineDash([])
 
@@ -124,8 +155,8 @@ function drawTextTest() {
  */
 function pickColor() {
     if (Math.random() < .2) {
-        return new color.Color(200, 16, 255);
+        return PURPLE;
     } else {
-        return new color.Color(0, 34, 51);
+        return BACKGROUND_COLOR;
     }
 }
